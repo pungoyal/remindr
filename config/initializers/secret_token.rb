c34569db2035d8bffcabc35e4bc7b1cee19a245e
Puneet Goyal <puneet@serenity.local>
2011-07-16 10:09:22 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Remindr::Application.config.secret_token = '3915958434970d33da510dee820bd8470328b75dc436cad3ea1c9a55dbbab8c80747b30e12991ab4792375ed9517e863596472c73573bddcdb049e426012e279'
