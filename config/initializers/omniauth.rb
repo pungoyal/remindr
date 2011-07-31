615f54dcde3d98828394a5d6abfac454a2b62cbb
Puneet Goyal <pungoyal@gmail.com>
2011-07-31 18:03:38 +0530

    scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :twitter, ENV['T_KEY'], ENV['T_SECRET']
   provider :facebook, ENV['F_KEY'], ENV['F_SECRET']
-  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id', :scope => 'email'
 end
